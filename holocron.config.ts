e377153e467f39f6a734c6d9ced82c336d3fea7d
Newton <5769156+iamnewton@users.noreply.github.com>
2026-07-20 20:18:48 -0700

    chore: fix description
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>

--- holocron.config.ts
+++ holocron.config.ts
@@ -5,7 +5,7 @@ import { node } from "@theholocron/holocron-config";
 const { repo, workflows, providers } = node();
 export default defineConfig({
 	description:
-		"API clients and shared HTTP primitives for theholocron tooling.",
+		"API clients and shared HTTP primitives.",
 	repo: {
 		...repo,
 		topics: [
